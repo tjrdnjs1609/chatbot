73516d63d4735021b9ae0506bf2568a79fbc38b4
oss201410509 <seokworking1609@gmail.com>
2019-12-13 08:24:12 +0000

    change token

--- v3/index.js
+++ v3/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { RTMClient } = require('@slack/rtm-api');
 
 
-const token = 'xoxb-519865642322-828717325828-zqjKrf5KEDSDMMElpvf32txP';
+const token = 'xoxb-519865642322-828717325828-9gOZybQ7j0vUZ4SmLWPSx2Uf';
 
 const rtm = new RTMClient(token);
 rtm.start();
